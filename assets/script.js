c458c6b00c2f9e5e9f4c0344035b68df89f9a006
itsmesimhaa <hongirana.s@cloudjournee.com>
2025-05-13 15:58:01 +0530

    Delete assets directory

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelectorAll('nav a').forEach(el => el.classList.remove('active'));
-    this.classList.add('active');
-  });
-});
